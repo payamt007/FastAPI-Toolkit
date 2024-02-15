5feef0d59bf52bfb586b61778c5a8c7f4e5ea7fd
p.taheri <p.taheri@emofid.com>
2024-02-15 09:36:39 +0330

    feat: added .env.test

--- app/db.py
+++ app/db.py
@@ -7,11 +7,12 @@ from sqlalchemy.ext.asyncio import create_async_engine
 
 from sqlmodel import SQLModel, create_engine
 
+db_driver = os.environ.get("DB_DRIVER")
 user = os.environ.get("POSTGRES_USER")
 password = os.environ.get("POSTGRES_PASSWORD")
 db = os.environ.get("POSTGRES_DB")
-# DATABASE_URL = "sqlite+aiosqlite:///database.db"
-DATABASE_URL = f"postgresql+asyncpg://{user}:{password}@pdb:5432/{db}"
+
+DATABASE_URL = f"{db_driver}{user}:{password}@pdb:5432/{db}"
 
 engine = create_async_engine(DATABASE_URL, echo=True, future=True)
 
