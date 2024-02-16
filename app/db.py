04b8ea90694356849e54881be53205ff73e3a109
p.taheri <p.taheri@emofid.com>
2024-02-16 10:42:59 +0330

    feat: Dynamically loaded db url in alembic.ini

--- app/db.py
+++ app/db.py
@@ -7,14 +7,8 @@ from sqlalchemy.ext.asyncio import create_async_engine
 
 from sqlmodel import SQLModel, create_engine
 
-db_driver = os.environ.get("DB_DRIVER")
-user = os.environ.get("POSTGRES_USER")
-password = os.environ.get("POSTGRES_PASSWORD")
-db = os.environ.get("POSTGRES_DB")
-db_host = os.environ.get("POSTGRES_HOST")
-
-DATABASE_URL = f"{db_driver}{user}:{password}@{db_host}:5432/{db}"
 # DATABASE_URL = "sqlite+aiosqlite:///database.db"
+DATABASE_URL = os.environ.get("DATABASE_URL")
 
 engine = create_async_engine(DATABASE_URL, echo=True, future=True)
 
--- migrations/env.py
+++ migrations/env.py
@@ -1,19 +1,20 @@
 import asyncio
+import os
 from logging.config import fileConfig
 
 from sqlalchemy import pool
 from sqlalchemy.engine import Connection
 from sqlalchemy.ext.asyncio import async_engine_from_config
-from sqlmodel import SQLModel                       # NEW
-
+from sqlmodel import SQLModel  # NEW
 
 from alembic import context
 
-from app.models import Song                         # NEW
+from app.models import Song  # NEW
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
+config.set_main_option('sqlalchemy.url', os.environ.get('DATABASE_URL'))
 
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.
@@ -24,7 +25,8 @@ if config.config_file_name is not None:
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
-target_metadata = SQLModel.metadata             # UPDATED
+target_metadata = SQLModel.metadata  # UPDATED
+
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
@@ -44,9 +46,9 @@ def run_migrations_offline() -> None:
     script output.
 
     """
-    url = config.get_main_option("sqlalchemy.url")
+    # url = config.get_main_option("sqlalchemy.url")
     context.configure(
-        url=url,
+        # url=url,
         target_metadata=target_metadata,
         literal_binds=True,
         dialect_opts={"paramstyle": "named"},
