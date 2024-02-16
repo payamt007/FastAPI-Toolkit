b4a1227f4370b6721f8b271ec28f0d7be29e3ade
p.taheri <p.taheri@emofid.com>
2024-02-16 17:21:21 +0330

    feat: Added foreign key to models

--- app/models.py
+++ app/models.py
@@ -5,12 +5,20 @@ from typing import Optional
 class SongBase(SQLModel):
     name: str
     artist: str
+    description: Optional[str] = None
     year: Optional[int] = None
 
 
 class Song(SongBase, table=True):
     id: int = Field(default=None, nullable=False, primary_key=True)
+    city_id: Optional[int] = Field(default=None, foreign_key="city.id")
 
 
 class SongCreate(SongBase):
     pass
+
+
+class City(SQLModel, table=True):
+    id: int = Field(default=None, nullable=False, primary_key=True)
+    title: str
+    desc: str
