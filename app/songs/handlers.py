152702d9963d740ebbae3ec0124927f97795bf81
p.taheri <p.taheri@emofid.com>
2024-02-17 19:53:57 +0330

    fix: updated description to song model

--- app/songs/handlers.py
+++ app/songs/handlers.py
@@ -12,12 +12,12 @@ router = APIRouter()
 async def get_songs(session: AsyncSession = Depends(get_session)):
     result = await session.execute(select(Song))
     songs = result.scalars().all()
-    return [Song(name=song.name, artist=song.artist, year=song.year, id=song.id) for song in songs]
+    return [Song(name=song.name, artist=song.artist, year=song.year, id=song.id, description=song.description) for song in songs]
 
 
 @router.post("/songs")
 async def add_song(song: SongCreate, session: AsyncSession = Depends(get_session)):
-    song = Song(name=song.name, artist=song.artist, year=song.year)
+    song = Song(name=song.name, artist=song.artist, year=song.year, description=song.description)
     session.add(song)
     await session.commit()
     await session.refresh(song)
