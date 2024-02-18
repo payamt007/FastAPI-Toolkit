51a0dd603c7d40142e03aa9606d79b9bc67c0cdc
p.taheri <p.taheri@emofid.com>
2024-02-18 20:13:08 +0330

    fix: added protected rout

--- app/songs/handlers.py
+++ app/songs/handlers.py
@@ -1,7 +1,11 @@
-from fastapi import APIRouter
+from typing import Annotated
+
+from fastapi import APIRouter, Depends
 from fastapi.security import OAuth2PasswordBearer
 from sqlmodel import Session, select
 
+from ..auth.handlers import get_current_active_user
+from ..auth.models import User
 from ..db import engine
 from .models import Song, SongCreate
 
@@ -11,7 +15,7 @@ oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
 
 
 @router.get("/songs", response_model=list[Song])
-async def get_songs():
+async def get_songs(current_user: Annotated[User, Depends(get_current_active_user)]):
     with Session(engine) as session:
         songs = session.exec(select(Song)).all()
         return [Song(name=song.name, artist=song.artist, year=song.year, id=song.id, description=song.description) for
